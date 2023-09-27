5aab5cc8dd2033469237ef62e5234c0a3041bcaa
Khalil Novaes <khalil.rocha@hotmail.com>
2023-09-27 01:37:56 -0300

    criação feature conversor de video para audio

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,6 +1,6 @@
 import {fastify} from 'fastify'
 import {fastifyCors} from '@fastify/cors'
-import { prisma } from './lib/prisma'
+// import { prisma } from './lib/prisma'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import {uploadVideoRoute} from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
